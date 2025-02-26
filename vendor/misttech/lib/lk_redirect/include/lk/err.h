dcbffbf564f909071e505c2c8c0d4f6dbc4a3458
Bruno Herrera <bruno.herrera@mist-os.com>
2025-02-26 08:44:25 -0300

    [mk][lk] LK Redirect to facilitate the porting of lk code
    Change-Id: Ic48a969d89f49a4d8be6f7693c3e88fb6875084a

---
+++ vendor/misttech/lib/lk_redirect/include/lk/err.h
@@ -0,0 +1,22 @@
+// Copyright 2025 Mist Tecnologia Ltda. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef VENDOR_MISTTECH_LIB_LK_REDIRECT_INCLUDE_LK_ERR_H_
+#define VENDOR_MISTTECH_LIB_LK_REDIRECT_INCLUDE_LK_ERR_H_
+
+#include <zircon/errors.h>
+#include <zircon/types.h>
+
+typedef zx_status_t status_t;
+
+#define NO_ERROR ZX_OK
+#define ERR_NOT_FOUND ZX_ERR_NOT_FOUND
+#define ERR_NO_RESOURCES ZX_ERR_NO_RESOURCES
+#define ERR_NOT_CONFIGURED ZX_ERR_UNAVAILABLE
+#define ERR_NOT_SUPPORTED ZX_ERR_NOT_SUPPORTED
+#define ERR_INVALID_ARGS ZX_ERR_INVALID_ARGS
+#define ERR_NOT_VALID ZX_ERR_BAD_HANDLE
+#define ERR_NO_MEMORY ZX_ERR_NO_MEMORY
+
+#endif  // VENDOR_MISTTECH_LIB_LK_REDIRECT_INCLUDE_LK_ERR_H_
